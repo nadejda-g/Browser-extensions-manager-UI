c0db3852b706b1e8f1b2f73a42fec7b1242d2402
nadejda-g <ndgolovaciuc@gmail.com>
2025-07-29 18:11:01 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Browser extensions manager UI solution
+
+This is a solution to the [Browser extensions manager UI challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/browser-extension-manager-ui-yNZnOfsMAp). Frontend Mentor challenge helped me improve my coding skills by building this realistic project. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author and Acknowledgements](#author)
+
+
+
+## Overview
+
+### The challenge
+
+Users are able to:
+
+- Toggle extensions between active and inactive states
+- Filter active and inactive extensions
+- Remove extensions from the list
+- Select their color theme
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![White Theme](./whiteHomePage.png)
+
+![Dark Theme](./darkHomePage.png)
+
+### Links
+
+- Example URL: [Frontend Mentor](https://www.frontendmentor.io/challenges/browser-extension-manager-ui-yNZnOfsMAp)
+- Solution/Live site URL: [Browser Extension Manager UI](https://www.frontendmentor.io/challenges/browser-extension-manager-ui-yNZnOfsMAp)
+
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Responsive design workflow
+
+
+### What I learned
+
+It was challenging for me to improve the interactive part of this website. All JavaScript implemented was a huge refresh of my knowleges. I've used fetching data from given data.json file to build all extensions card and their description. Also added interactive features using event listeners  and other facilities offered by js.
+
+### Continued development
+
+Going to improve my js skills and focus more on learning js frameworks and libraries.
+
+### Useful resources
+
+- [W3schools](https://www.w3schools.com/) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
+- [CSS Toggles](https://csstoggles.github.io/)  which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
+- [CSS Scan](https://getcssscan.com/css-box-shadow-examples)
+- [Bootstrap](https://getbootstrap.com/)
+- [MDN Documentation](https://developer.mozilla.org/)
+
+
+## Authors and Acknowledgements
+
+- Author - [@nadejda-g](https://github.com/nadejda-g)
+- Template - [@Frontend Mentor](https://www.frontendmentor.io)
+- Mentor - [@dragomirt](https://github.com/dragomirt)
---
+++ browser-extensions.js
@@ -0,0 +1,142 @@
+async function fetchData() {
+    const res = await fetch('/data.json')
+    const data = await res.json()
+
+    renderCards(data)
+}
+
+function displayOnlyActive() {
+    const cardsContainer = document.getElementById('cards-container')
+    cardsContainer.classList.add('only-active')
+    cardsContainer.classList.remove('only-inactive')
+}
+
+function displayOnlyInactive() {
+    const cardsContainer = document.getElementById('cards-container')
+    cardsContainer.classList.add('only-inactive')
+    cardsContainer.classList.remove('only-active')
+}
+
+function displayAll() {
+    const cardsContainer = document.getElementById('cards-container')
+    cardsContainer.classList.remove('only-active')
+    cardsContainer.classList.remove('only-inactive')
+}
+
+function removeCard(event) {
+    const button = event.target
+    const footer = button.parentElement
+    const content = footer.parentElement
+    const generalCard = content.parentElement
+
+    generalCard.classList.add('is-removed')
+}
+
+function toggleCard(event) {
+    const input = event.target
+    const checked = input.checked
+
+    const toggle = input.parentElement
+    const footer = toggle.parentElement
+    const content = footer.parentElement
+    const generalCard = content.parentElement
+
+    if (checked) {
+        generalCard.classList.add('is-active')
+        generalCard.classList.remove('is-inactive')
+    } else {
+        generalCard.classList.add('is-inactive')
+        generalCard.classList.remove('is-active')
+    }
+}
+
+function switchColors() {
+    const body = document.querySelector('body')
+    const isDarkMode = body.classList.contains('dark-mode')
+    const image = document.querySelector('.navbar button img')
+
+    if (isDarkMode) {
+        body.classList.remove('dark-mode')
+        image.src = '/assets/images/icon-moon.svg'
+    } else {
+        body.classList.add('dark-mode')
+        image.src = '/assets/images/icon-sun.svg'
+    }
+}
+
+function renderCards(data) {
+    for (const card of data) {
+        renderCard(card)
+    }
+}
+
+function renderCard(card) {
+    const cardsContainer = document.getElementById('cards-container')
+
+    const generalCard = document.createElement('div')
+    generalCard.classList.add('general-card')
+
+    const cardContent = document.createElement('div')
+    cardContent.classList.add('card-content')
+
+    const cardHeader = document.createElement('div')
+    cardHeader.classList.add('card-header')
+
+    const cardFooter = document.createElement('div')
+    cardFooter.classList.add('card-footer')
+
+    const cardDescription = document.createElement('div')
+    cardDescription.classList.add('card-description')
+
+    const title = document.createElement('h3')
+    title.innerText = card.name
+
+    const description = document.createElement('p')
+    description.innerText = card.description
+
+    const image = document.createElement('img')
+    image.src = card.logo
+
+
+    const button = document.createElement('button')
+    button.innerText = "Remove"
+    button.addEventListener('click', removeCard)
+
+
+    const toggle = document.createElement('div')
+    toggle.classList.add('toggle')
+    toggle.addEventListener('click', toggleCard)
+
+    const input = document.createElement('input')
+    input.setAttribute('type', 'checkbox')
+
+    if (card.isActive) {
+        input.setAttribute('checked', true)
+
+        generalCard.classList.add('is-active')
+    } else {
+        generalCard.classList.add('is-inactive')
+    }
+
+    const label = document.createElement('label')
+
+    cardDescription.appendChild(title)
+    cardDescription.appendChild(description)
+
+    cardHeader.appendChild(image)
+    cardHeader.appendChild(cardDescription)
+
+    cardContent.appendChild(cardHeader)
+    cardContent.appendChild(cardFooter)
+
+    toggle.appendChild(input)
+    toggle.appendChild(label)
+
+    cardFooter.appendChild(button)
+    cardFooter.appendChild(toggle)
+
+    generalCard.appendChild(cardContent)
+    cardsContainer.appendChild(generalCard)
+}
+
+fetchData()
